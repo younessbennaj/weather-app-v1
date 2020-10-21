aaa9a6f066804a523528b4b544711f2c44512c28
Youness BENNAJ <youness.bennaj@gmail.com>
2020-10-21 10:47:53 +0200

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    // need absolute path
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
